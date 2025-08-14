b5deed09310599eba96456ec35d5aae03d12fe53
SEO GEONHYUK <sgh4123@plitsoft.com>
2025-08-14 10:33:24 +0900

    fix: bundle ID package.json에 설정하는 것으로 변경

--- forge.config.mjs
+++ forge.config.mjs
@@ -16,7 +16,6 @@ export default {
   ],
   packagerConfig: {
     osxSign: {
-      appBundleId: "com.plitsoft.stock5misago",
       identity: process.env.DEVELOPER_ID_APPLICATION
     },
     osxNotarize: {
